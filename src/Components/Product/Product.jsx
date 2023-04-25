602177bdda8988fec22cb640aaee2787d07ce918
MustafaRamadan74 <mafroto74@gmail.com>
2023-04-25 21:10:58 +0200

    update

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -18,6 +18,7 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Trending</p>
             </div>
             <a href={"https://mustafaramadan74.github.io/Trending/"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={Trending} alt="it's a project" />
@@ -28,7 +29,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Caravan Store</p>
             </div>
+            
             <a href={"https://github.com/MustafaRamadan74"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={caravan} alt="it's a project" />
             </a>
@@ -38,7 +41,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Yummy</p>
             </div>
+            
             <a href={"https://mustafaramadan74.github.io/Yummy/"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={Yummy} alt="it's a project" />
             </a>
@@ -48,7 +53,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Portfolio</p>
             </div>
+            
             <a href={"https://github.com/MustafaRamadan74"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={project3} alt="it's a project" />
             </a>
@@ -58,7 +65,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Levelling calculator</p>
             </div>
+            
             <a href={"https://mustafaramadan74.github.io/Levelling-app/"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={levillingApp} alt="it's a project" />
             </a>
@@ -68,7 +77,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Project Management System</p>
             </div>
+            
             <a href={"https://mustafaramadan74.github.io/pharmacy/"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={PharmacyCRUD} alt="it's a project" />
             </a>
@@ -78,7 +89,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Traverse Adjustment</p>
             </div>
+            
             <a href={"https://mustafaramadan74.github.io/Traverse-Adjustment/"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={traverseAdj} alt="it's a project" />
             </a>
@@ -88,7 +101,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Portfolio</p>
             </div>
+            
             <a href={"https://github.com/MustafaRamadan74"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={project1} alt="it's a project" />
             </a>
@@ -98,7 +113,9 @@ export default function Product() {
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
                 <div className="p-circle"></div>
+                <p className='mx-4 my-auto text-dark fw-bolder'>Quiz App</p>
             </div>
+            
             <a href={"https://mustafaramadan74.github.io/Quiz-App/"} target="_blank" rel='noreferrer'>
                 <img className='img-project' src={QuizApp} alt="it's a project" />
             </a>
